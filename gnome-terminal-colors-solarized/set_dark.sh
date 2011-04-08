1ccdaa55104adfc891272892c39f62c166cc1021
Ticean Bennett <ticean@gmail.com>
2011-04-07 20:47:23 -0700

    Modified the profile name.

--- gnome-terminal-colors-solarized/set_dark.sh
+++ gnome-terminal-colors-solarized/set_dark.sh
@@ -3,9 +3,9 @@
 dir=`dirname $0`
 
 # set palette
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette `cat $dir/colors/palette`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Solarized-Dark1/palette `cat $dir/colors/palette`
 
 # set foreground to base0 and background to base03 and highlight color to base1
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color `cat $dir/colors/base03`
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color `cat $dir/colors/base0`
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/bold_color `cat $dir/colors/base1`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Solorized-Dark1/background_color `cat $dir/colors/base03`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Solarized-Dark1/foreground_color `cat $dir/colors/base0`
+gconftool-2 -s -t string /apps/gnome-terminal/profiles/Solarized-Dark1/bold_color `cat $dir/colors/base1`
